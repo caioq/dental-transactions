b2f0e6fa41e93396ac63e2eb37d113770867c293
Caio Queiroz <caiogqueiroz@gmail.com>
2024-03-11 13:45:33 -0300

    feat: pending payments endpoint

--- backend/src/domain/procedure/repositories/procedure.repository.ts
+++ backend/src/domain/procedure/repositories/procedure.repository.ts
@@ -7,9 +7,14 @@ export interface ProceduresFilter {
   }
 }
 
+export interface ProcedurePendingPayments extends Procedure {
+  totalPayment: number
+}
+
 export abstract class ProcedureRepository {
   abstract create(procedure: Procedure): Promise<Procedure>
   abstract update(procedure: Procedure): Promise<Procedure>
   abstract findById(id: string): Promise<Procedure | null>
   abstract findByDoctorId(doctorId: string, filter?: ProceduresFilter): Promise<Procedure[]>
+  abstract findProceduresWithPendingPayments(doctorId: string): Promise<ProcedurePendingPayments[]>
 }
